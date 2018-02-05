99ecec17f586ab46269e27449d4052f77bf7032e
German <magicnova@hotmail.com>
2018-02-05 10:04:10 -0300

    estructura inicial

---
+++ Distributor.Domain/Brands.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Distributor.Domain
+{
+    public enum Brands
+    {
+        [Description("Ford")]
+        Ford=0,
+        [Description("Toyota")]
+        Toyota=1
+    }
+}
