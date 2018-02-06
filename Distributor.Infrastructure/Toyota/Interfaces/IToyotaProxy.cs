7fd41dd0cf24e8e89fdd1b71872c8a6c31d11f44
German <magicnova@hotmail.com>
2018-02-06 06:37:32 -0300

    add toyota proxy methods

--- Distributor.Infrastructure/Toyota/Interfaces/IToyotaProxy.cs
+++ Distributor.Infrastructure/Toyota/Interfaces/IToyotaProxy.cs
@@ -7,5 +7,11 @@ namespace Distributor.Infrastructure.Toyota.Interfaces
     public interface IToyotaProxy
     {
         IList<Car> GetAll(ToyotaConfiguration configuration);
+        IList<Car> GetByModel(ToyotaConfiguration configuration, string model);
+        IList<Car> GetByTransmission(ToyotaConfiguration configuration, string transmission);
+        IList<Car> GetByEngine(ToyotaConfiguration configuration, string engine);
+        IList<Car> GetByYear(ToyotaConfiguration configuration, int year);
+        IList<Car> GetById(ToyotaConfiguration configuration, int id);
+        
     }
 }
--- Distributor.Infrastructure/Toyota/ToyotaProxy.cs
+++ Distributor.Infrastructure/Toyota/ToyotaProxy.cs
@@ -30,6 +30,56 @@ namespace Distributor.Infrastructure.Toyota
             return _toyotaMapper.MapCarsToDomain(cars);
         }
 
+        public IList<Car> GetByModel(ToyotaConfiguration configuration, string model)
+        {
+            var headerValue = GetHeader(configuration);
+            var url = $"{GetUrl("GET_BY_MODEL",configuration)}{model}";
+
+            var cars = _httpClient.Get(url, headerValue, "BASIC");
+
+            return _toyotaMapper.MapCarsToDomain(cars);
+        }
+
+        public IList<Car> GetByTransmission(ToyotaConfiguration configuration, string transmission)
+        {
+            var headerValue = GetHeader(configuration);
+            var url = $"{GetUrl("GET_BY_TRANSMISSION", configuration)}{transmission}";
+            
+            var cars = _httpClient.Get(url, headerValue, "BASIC");
+
+            return _toyotaMapper.MapCarsToDomain(cars);
+        }
+
+        public IList<Car> GetByEngine(ToyotaConfiguration configuration, string engine)
+        {
+            var headerValue = GetHeader(configuration);
+            var url = $"{GetUrl("GET_BY_ENGINE",configuration)}{engine}";
+
+            var cars = _httpClient.Get(url, headerValue, "BASIC");
+
+            return _toyotaMapper.MapCarsToDomain(cars);     
+        }
+
+        public IList<Car> GetByYear(ToyotaConfiguration configuration, int year)
+        {
+            var headerValue = GetHeader(configuration);
+            var url = $"{GetUrl("GET_BY_YEAR",configuration)}{year}";
+
+            var cars = _httpClient.Get(url, headerValue, "BASIC");
+
+            return _toyotaMapper.MapCarsToDomain(cars);     
+        }
+
+        public IList<Car> GetById(ToyotaConfiguration configuration, int id)
+        {
+            var headerValue = GetHeader(configuration);
+            var url = $"{GetUrl("GET_BY_ID",configuration)}{id}";
+
+            var cars = _httpClient.Get(url, headerValue, "BASIC");
+
+            return _toyotaMapper.MapCarsToDomain(cars);     
+        }
+
         private string GetUrl(string action, ToyotaConfiguration configuration)
         {
             var baseUrl = configuration.BaseUrl;
